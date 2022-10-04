6320f0f32b4b67efadd026f2eb11ad6421ca11b4
tbhering <tmbhering@gmail.com>
2022-10-03 21:57:43 -0300

    Atualização inputs 30%

---
+++ validacao-doguito-main/assets/js/validacao.js
@@ -0,0 +1,36 @@
+export function valida(input){
+    const tipoDeInput = input.dataset.tipo
+
+    if(validadores[tipoDeInput]){
+        validadores[tipoDeInput](input)
+    }
+}
+
+const validadores = {
+    dataNascimento:input => validaDataNascimento(input)
+}
+
+/*const dataNascimento = document.querySelector('#nascimento')
+
+dataNascimento.addEventListener('blur', (evento) => {
+    validaDataNascimento(evento.target)
+})*/
+
+function validaDataNascimento(input) {
+    const dataRecebida = new Date(input.value)
+    let mensagem = ''
+
+    if(!maiorq18(dataRecebida)){
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+
+function maiorq18(data){
+    const dataAtual = new Date()
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
+
+    return dataMais18 <= dataAtual
+}
